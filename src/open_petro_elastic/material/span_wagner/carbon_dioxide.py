acc0417d967703be63c10a54b1ce409d9eb21a0e
Vegard Berg Kvernelv <vegard@nr.no>
2021-08-24 20:15:23 +0200

    Moved coefficients to a separate file. Changed how the residual helmholtz energy is calculated: instead of relying on copy-pasting output from code in the docstring of carbon_dioxide.py, we now perform the sympy evaluations when the module is imported. Further, the expressions are "lambdified" by sympy and can thus be evaluated with numpy arrays as input.

--- src/open_petro_elastic/material/span_wagner/carbon_dioxide.py
+++ src/open_petro_elastic/material/span_wagner/carbon_dioxide.py
@@ -1,127 +1,12 @@
 """
-Equation 6.1 from Span & Wagner [2], and its derivatives have been computed using sympy and the following code:
-
-First, define the kernels of the summations in the Helmholtz energy residual function of Table 32:
-    >>> import sympy as sp
-    ... (n1, t1, d1, n2, d2, t2, c2, n3, d3, t3, alpha3, epsilon3, beta3, gamma3, n4, b4, a4, beta4, A4, B4, C4, D4) =\
-    ...     sp.symbols('n1 t1 d1 n2 d2 t2 c2 n3 d3 t3 alpha3 epsilon3 beta3 gamma3 n4 b4 a4 beta4 A4 B4 C4 D4')
-    ... tau, delta = sp.symbols('tau delta', real=True)
-    ... i = sp.symbols('i', integer=True)
-    ... s1 = n1 * delta ** d1 * tau ** t1
-    ... s2 = n2 * delta ** d2 * tau ** t2 * sp.exp(-delta ** c2)
-    ... s3 = n3 * delta ** d3 * tau ** t3 * sp.exp(-alpha3 * (delta - epsilon3) **2 - beta3 * (tau - gamma3)**2)
-    ...
-    ... theta, bigdelta, bigphi = sp.symbols('theta bigdelta bigphi')
-    ... theta_f, bigdelta_f, bigphi_f = sp.symbols('theta bigdelta bigphi', cls=sp.Function)
-    ...
-    ... theta_expr = (1 - tau) + A4 * ((delta - 1) ** 2) ** (1 / (2 * beta4))
-    ... bigdelta_expr = theta_f(delta, tau) ** 2 + B4 * ((delta - 1) ** 2) ** a4
-    ... bigphi_expr = sp.exp(-C4 * (delta - 1) ** 2 - D4 * (tau - 1) ** 2)
-    ... s4 = n4 * bigdelta_f(delta, tau) ** b4 * delta * bigphi_f(delta, tau)
-    ...
-    ... # Print the expressions and its derivatives in a manner consistent with the code below
-    ... # Define derivatives as symbols to enable printing to code
-    ... dt_theta, dd_theta = sp.symbols('dt_theta dd_theta')
-    ... dt_bigdelta, dd_bigdelta = sp.symbols('dt_bigdelta dd_bigdelta')
-    ... dt_bigphi, dd_bigphi = sp.symbols('dt_bigphi dd_bigphi')
-    ...
-    ... dtt_theta, ddt_theta = sp.symbols('dtt_theta ddt_theta')
-    ... dtt_bigdelta, ddt_bigdelta = sp.symbols('dtt_bigdelta ddt_bigdelta')
-    ... dtt_bigphi, ddt_bigphi = sp.symbols('dtt_bigphi ddt_bigphi')
-    ...
-    ... ddd_theta = sp.symbols('ddd_theta')
-    ... ddd_bigdelta = sp.symbols('ddd_bigdelta')
-    ... ddd_bigphi = sp.symbols('ddd_bigphi')
-    ...
-    ... def print_expr(name, e, summation):
-    ...     e = e.xreplace({
-    ...         theta_f(delta, tau): theta,
-    ...         bigdelta_f(delta, tau): bigdelta,
-    ...         bigphi_f(delta, tau): bigphi,
-    ...         theta_f(delta, tau).diff(tau): dt_theta,
-    ...         theta_f(delta, tau).diff(delta): dd_theta,
-    ...         bigdelta_f(delta, tau).diff(tau): dt_bigdelta,
-    ...         bigdelta_f(delta, tau).diff(delta): dd_bigdelta,
-    ...         bigphi_f(delta, tau).diff(tau): dt_bigphi,
-    ...         bigphi_f(delta, tau).diff(delta): dd_bigphi,
-    ...         theta_f(delta, tau).diff(tau, tau): dtt_theta,
-    ...         theta_f(delta, tau).diff(delta, tau): ddt_theta,
-    ...         bigdelta_f(delta, tau).diff(tau, tau): dtt_bigdelta,
-    ...         bigdelta_f(delta, tau).diff(delta, tau): ddt_bigdelta,
-    ...         bigphi_f(delta, tau).diff(tau, tau): dtt_bigphi,
-    ...         bigphi_f(delta, tau).diff(delta, tau): ddt_bigphi,
-    ...         theta_f(delta, tau).diff(delta, delta): ddd_theta,
-    ...         bigdelta_f(delta, tau).diff(delta, delta): ddd_bigdelta,
-    ...         bigphi_f(delta, tau).diff(delta, delta): ddd_bigphi,
-    ...     })
-    ...     e = e.powsimp()
-    ...     kernel = str(e).replace("Abs", "abs")
-    ...     if summation:
-    ...         print(f'{name} = sum({kernel}, axis=-1)')
-    ...     else:
-    ...         print(f'{name} = {kernel}')
-    ...
-    ... sep = '=' * 30
-    ...
-    ...
-    ... # Base functions:
-    ... print_expr('theta', theta_expr, False)
-    ... print_expr('bigdelta', bigdelta_expr, False)
-    ... print_expr('bigphi', bigphi_expr, False)
-    ... for i, s in enumerate([s1, s2, s3, s4]):
-    ...     print_expr(f's{i+1}', s, True)
-    ... print(sep)
-    ...
-    ... # First derivatives
-    ... print_expr('dt_theta', theta_expr.diff(tau), False)
-    ... print_expr('dt_bigdelta', bigdelta_expr.diff(tau), False)
-    ... print_expr('dt_bigphi', bigphi_expr.diff(tau), False)
-    ... for i, s in enumerate([s1, s2, s3, s4]):
-    ...     print_expr(f'dt_s{i+1}', s.diff(tau), True)
-    ... print(sep)
-    ... print_expr('dd_theta', theta_expr.diff(delta), False)
-    ... print_expr('dd_bigdelta', bigdelta_expr.diff(delta), False)
-    ... print_expr('dd_bigphi', bigphi_expr.diff(delta), False)
-    ... for i, s in enumerate([s1, s2, s3, s4]):
-    ...     print_expr(f'dd_s{i+1}', s.diff(delta), True)
-    ... print(sep)
-    ...
-    ... # Second derivatives
-    ... print_expr('dtt_theta', theta_expr.diff(tau, tau), False)
-    ... print_expr('dtt_bigdelta', bigdelta_expr.diff(tau, tau), False)
-    ... print_expr('dtt_bigphi', bigphi_expr.diff(tau, tau), False)
-    ... for i, s in enumerate([s1, s2, s3, s4]):
-    ...     print_expr(f'dtt_s{i+1}', s.diff(tau, tau), True)
-    ... print(sep)
-    ... print_expr('ddd_theta', theta_expr.diff(delta, delta), False)
-    ... print_expr('ddd_bigdelta', bigdelta_expr.diff(delta, delta), False)
-    ... print_expr('ddd_bigphi', bigphi_expr.diff(delta, delta), False)
-    ... for i, s in enumerate([s1, s2, s3, s4]):
-    ...     print_expr(f'ddd_s{i+1}', s.diff(delta, delta), True)
-    ... print(sep)
-    ... print_expr('ddt_theta', theta_expr.diff(delta, tau), False)
-    ... print_expr('ddt_bigdelta', bigdelta_expr.diff(delta, tau), False)
-    ... print_expr('ddt_bigphi', bigphi_expr.diff(delta, tau), False)
-    ... for i, s in enumerate([s1, s2, s3, s4]):
-    ...     print_expr(f'ddt_s{i+1}', s.diff(delta, tau), True)
-    ... print(sep)
-
-The expressions are directly equivalent to the expressions in the code below, with an exception being sympy's DiracDelta
-function. This can, however, be directly replaced with a boolean expression.
+Package for calculating CO2 properties based on Span & Wagner [2].
 """
 import numpy as np
-from numpy import array, log, exp, sum, abs, sign
+from numpy import log, exp, sum
 from open_petro_elastic import float_vectorize
-
 from open_petro_elastic.material.fluid import fluid_material as fluid
-from open_petro_elastic.material.conversions import celsius_to_kelvin
-
-
-"""
-TODO:
-    - Handle density (and other derived properties) close to phase transitions, critical point and triple point
-    - Support vectorized variants of the below functions
-"""
+from open_petro_elastic.material.span_wagner.coefficients import a0, theta0
+from open_petro_elastic.material.span_wagner import equations
 
 
 # Constants
@@ -131,113 +16,6 @@ CO2_CRITICAL_DENSITY = 467.6  # kg / m^3
 CO2_CRITICAL_PRESSURE = 7.3773  # MPa
 CO2_TRIPLE_TEMPERATURE = 216.592  # K
 CO2_TRIPLE_PRESSURE = 0.51795  # MPa
-
-
-"""
-Span-Wagner Coefficients
-
-Coefficients defined in Table 31 of Span & Wagner. Coefficients are divided into groups according to horizontal lines in
-the table. This is also convenient when evaluating the Helmholtz energy expressions (phi functions).
-"""
-
-
-""" Ideal gas part """
-a0 = array([[
-    8.37304456,
-    -3.70454304,
-    2.50000000,
-    1.99427042,
-    0.62105248,
-    0.41195293,
-    1.04028922,
-    0.08327678,
-]])
-theta0 = array([[
-    3.15163,
-    6.11190,
-    6.77708,
-    11.32384,
-    27.08792,
-]])
-
-
-""" Residual part """
-# First group
-n1 = array([[
-    0.38856823203161 * 1e0,
-    0.29385475942740 * 1e1,
-    -0.55867188534934 * 1e1,
-    -0.76753199592477 * 1e0,
-    0.31729005580416 * 1e0,
-    0.54803315897767 * 1e0,
-    0.12279411220335 * 1e0,
-]])
-d1 = array([[1, 1, 1, 1, 2, 2, 3]])
-t1 = array([[0.00, 0.75, 1.00, 2.00, 0.75, 2.00, 0.75]])
-
-# Second group
-n2 = array([[
-    0.21658961543220 * 1e1,
-    0.15841735109724 * 1e1,
-    -0.23132705405503 * 1e0,
-    0.58116916431436 * 1e-1,
-    -0.55369137205382 * 1e0,
-    0.48946615909422 * 1e0,
-    -0.24275739843501 * 1e-1,
-    0.62494790501678 * 1e-1,
-    -0.12175860225246 * 1e0,
-    -0.37055685270086 * 1e0,
-    -0.16775879700426 * 1e-1,
-    -0.11960736637987 * 1e0,
-    -0.45619362508778 * 1e-1,
-    0.35612789270346 * 1e-1,
-    -0.74427727132052 * 1e-2,
-    -0.17395704902432 * 1e-2,
-    -0.21810121289527 * 1e-1,
-    0.24332166559236 * 1e-1,
-    -0.37440133423463 * 1e-1,
-    0.143387157568781 * 1e0,
-    -0.13491969083286 * 1e0,
-    -0.23151225053480 * 1e-1,
-    0.12363125492901 * 1e-1,
-    0.21058321972940 * 1e-2,
-    -0.33958519026368 * 1e-3,
-    0.55993651771592 * 1e-2,
-    -0.30335118055646 * 1e-3,
-]])
-d2 = array([[1, 2, 4, 5, 5, 5, 6, 6, 6, 1, 1, 4, 4, 4, 7, 8, 2, 3, 3, 5, 5, 6, 7, 8, 10, 4, 8]])
-t2 = array([[1.50, 1.50, 2.50, 0.00, 1.50, 2.00, 0.00, 1.00, 2.00, 3.00, 6.00, 3.00, 6.00, 8.00, 6.00, 0.00, 7.00, 12.00,
-             16.00, 22.00, 24.00, 16.00, 24.00, 8.00, 2.00, 28.00, 14.00]])
-c2 = array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 6]])
-
-# Third group
-n3 = array([[
-    -0.21365488688320 * 1e3,
-    0.26641569149272 * 1e5,
-    -0.24027212204557 * 1e5,
-    -0.28341603423999 * 1e3,
-    0.21247284400179 * 1e3,
-]])
-d3 = array([[2, 2, 2, 3, 3]])
-t3 = array([[1.00, 0.00, 1.00, 3.00, 3.00]])
-alpha3 = array([[25, 25, 25, 15, 20]])
-beta3 = array([[325, 300, 300, 275, 275]])
-gamma3 = array([[1.16, 1.19, 1.19, 1.25, 1.22]])
-epsilon3 = array([[1.00, 1.00, 1.00, 1.00, 1.00]])
-
-# Fourth group
-n4 = array([[
-    -0.66642276540751 * 1e0,
-    0.72608632349897 * 1e0,
-    0.55068668612842 * 1e-1,
-]])
-a4 = array([[3.500, 3.500, 3.000]])
-b4 = array([[0.875, 0.925, 0.875]])
-beta4 = array([[0.300, 0.300, 0.300]])
-A4 = array([[0.700, 0.700, 0.700]])
-B4 = array([[0.3, 0.3, 1.0]])
-C4 = array([[10.0, 10.0, 12.5]])
-D4 = array([[275, 275, 275]])
 
 
 def co2_helmholtz_energy(delta, tau, dd, dt):
@@ -288,8 +66,8 @@ def ideal_gas_helmholtz_energy(delta, tau, dd, dt):
 
 def co2_residual_helmholtz_energy(delta, tau, dd, dt):
     """
-    Residual part of Helmholtz energy as defined by the equation in Table 32 of Span & Wagner [2]. See function phi for
-    argument documentation.
+    Residual part of Helmholtz energy as defined by the equation in Table 32 of Span & Wagner [2]. See
+    co2_helmholtz_energy for argument documentation.
     """
     tau = np.asarray(tau)
     delta = np.asarray(delta)
@@ -300,103 +78,8 @@ def co2_residual_helmholtz_energy(delta, tau, dd, dt):
     tau[tau == 1.0] -= 1e-15
     delta[delta == 1.0] -= 1e-15
 
-    theta = A4*abs(delta - 1)**(1/beta4) - tau + 1
-    bigdelta = B4*abs(delta - 1)**(2*a4) + theta**2
-    bigphi = exp(-C4*(delta - 1)**2 - D4*(tau - 1)**2)
-    if dt == dd == 0:
-        s1 = sum(delta**d1*n1*tau**t1, axis=-1)
-        s2 = sum(delta**d2*n2*tau**t2*exp(-delta**c2), axis=-1)
-        s3 = sum(delta**d3*n3*tau**t3*exp(-alpha3*(delta - epsilon3)**2 - beta3*(-gamma3 + tau)**2), axis=-1)
-        s4 = sum(bigdelta**b4*bigphi*delta*n4, axis=-1)
-        res = s1 + s2 + s3 + s4
-    elif dt == 1 and dd == 0:
-        dt_theta = -1
-        dt_bigdelta = 2*dt_theta*theta
-        dt_bigphi = -D4*(2*tau - 2)*exp(-C4*(delta - 1)**2 - D4*(tau - 1)**2)
-        dt_s1 = sum(delta**d1*n1*t1*tau**(t1 - 1), axis=-1)
-        dt_s2 = sum(delta**d2*n2*t2*tau**(t2 - 1)*exp(-delta**c2), axis=-1)
-        dt_s3 = sum(-beta3*delta**d3*n3*tau**t3*(-2*gamma3 + 2*tau)*exp(-alpha3*(delta - epsilon3)**2 - beta3*(
-                -gamma3 + tau)**2) + delta**d3*n3*t3*tau**(t3 - 1)*exp(-alpha3*(delta - epsilon3)**2 - beta3*(-gamma3 +
-                                                                                                              tau)**2),
-                    axis=-1)
-        dt_s4 = sum(b4*bigdelta**(b4 - 1)*bigphi*delta*dt_bigdelta*n4 + bigdelta**b4*delta*dt_bigphi*n4)
-        res = dt_s1 + dt_s2 + dt_s3 + dt_s4
-    elif dt == 0 and dd == 1:
-        dd_theta = A4 * abs(delta - 1) ** (-1 + 1 / beta4) * sign(delta - 1) / beta4
-        dd_bigdelta = 2 * B4 * a4 * abs(delta - 1) ** (2 * a4 - 1) * sign(delta - 1) + 2 * dd_theta * theta
-        dd_bigphi = -C4 * (2 * delta - 2) * exp(-C4 * (delta - 1) ** 2 - D4 * (tau - 1) ** 2)
-        dd_s1 = sum(d1 * delta ** (d1 - 1) * n1 * tau ** t1, axis=-1)
-        dd_s2 = sum(-c2 * delta ** (c2 + d2 - 1) * n2 * tau ** t2 * exp(-delta ** c2) + d2 * delta ** (
-                    d2 - 1) * n2 * tau ** t2 * exp(-delta ** c2), axis=-1)
-        dd_s3 = sum(-alpha3 * delta ** d3 * n3 * tau ** t3 * (2 * delta - 2 * epsilon3) * exp(
-            -alpha3 * (delta - epsilon3) ** 2 - beta3 * (-gamma3 + tau) ** 2) + d3 * delta ** (
-                                d3 - 1) * n3 * tau ** t3 * exp(
-            -alpha3 * (delta - epsilon3) ** 2 - beta3 * (-gamma3 + tau) ** 2), axis=-1)
-        dd_s4 = sum(b4 * bigdelta ** (
-                    b4 - 1) * bigphi * dd_bigdelta * delta * n4 + bigdelta ** b4 * bigphi * n4 + bigdelta ** b4 *
-                    dd_bigphi * delta * n4, axis=-1)
-        res = dd_s1 + dd_s2 + dd_s3 + dd_s4
-    else:
-        dt_theta = -1
-        dt_bigdelta = 2*dt_theta*theta
-        dt_bigphi = -D4*(2*tau - 2)*exp(-C4*(delta - 1)**2 - D4*(tau - 1)**2)
-        dd_theta = A4 * abs(delta - 1) ** (-1 + 1 / beta4) * sign(delta - 1) / beta4
-        dd_bigdelta = 2 * B4 * a4 * abs(delta - 1) ** (2 * a4 - 1) * sign(delta - 1) + 2 * dd_theta * theta
-        dd_bigphi = -C4 * (2 * delta - 2) * exp(-C4 * (delta - 1) ** 2 - D4 * (tau - 1) ** 2)
-        if dt == 2 and dd == 0:
-            dtt_theta = 0
-            dtt_bigdelta = 2 * dt_theta ** 2 + 2 * dtt_theta * theta
-            dtt_bigphi = 2 * D4 * (2 * D4 * (tau - 1) ** 2 - 1) * exp(-C4 * (delta - 1) ** 2 - D4 * (tau - 1) ** 2)
-            dtt_s1 = sum(delta ** d1 * n1 * t1 * tau ** (t1 - 2) * (t1 - 1), axis=-1)
-            dtt_s2 = sum(delta ** d2 * n2 * t2 * tau ** (t2 - 2) * (t2 - 1) * exp(-delta ** c2), axis=-1)
-            dtt_s3 = sum(delta ** d3 * n3 * tau ** t3 * (4 * beta3 * t3 * (gamma3 - tau) / tau + 2 * beta3 * (
-                        2 * beta3 * (gamma3 - tau) ** 2 - 1) + t3 * (t3 - 1) / tau ** 2) * exp(
-                -alpha3 * (delta - epsilon3) ** 2 - beta3 * (-gamma3 + tau) ** 2), axis=-1)
-            dtt_s4 = sum(bigdelta ** b4 * delta * n4 * (b4 * bigphi * (
-                        b4 * dt_bigdelta ** 2 / bigdelta + dtt_bigdelta - dt_bigdelta ** 2 / bigdelta) / bigdelta + 2 *
-                                                        b4 * dt_bigdelta * dt_bigphi / bigdelta + dtt_bigphi), axis=-1)
-            res = dtt_s1 + dtt_s2 + dtt_s3 + dtt_s4
-        elif dt == 0 and dd == 2:
-            ddd_theta = A4 * (
-                        2 * (delta == 1) / abs(delta - 1) - sign(delta - 1) ** 2 / (delta - 1) ** 2 + sign(
-                    delta - 1) ** 2 / (beta4 * (delta - 1) ** 2)) * abs(delta - 1) ** (1 / beta4) / beta4
-            ddd_bigdelta = 4 * B4 * a4 ** 2 * abs(delta - 1) ** (2 * a4) * sign(delta - 1) ** 2 / (
-                        delta - 1) ** 2 + 4 * B4 * a4 * abs(delta - 1) ** (2 * a4 - 1) * (delta == 1) - 2 * B4 * a4 *\
-                           abs(delta - 1) ** (2 * a4) * sign(delta - 1) ** 2 / (
-                                       delta - 1) ** 2 + 2 * dd_theta ** 2 + 2 * ddd_theta * theta
-            ddd_bigphi = 2 * C4 * (2 * C4 * (delta - 1) ** 2 - 1) * exp(-C4 * (delta - 1) ** 2 - D4 * (tau - 1) ** 2)
-            ddd_s1 = sum(d1 * delta ** (d1 - 2) * n1 * tau ** t1 * (d1 - 1), axis=-1)
-            ddd_s2 = sum(delta ** (d2 - 2) * n2 * tau ** t2 * (
-                        -2 * c2 * d2 * delta ** c2 + c2 * delta ** c2 * (c2 * delta ** c2 - c2 + 1) + d2 * (
-                            d2 - 1)) * exp(-delta ** c2), axis=-1)
-            ddd_s3 = sum(delta ** d3 * n3 * tau ** t3 * (-4 * alpha3 * d3 * (delta - epsilon3) / delta + 2 * alpha3 * (
-                        2 * alpha3 * (delta - epsilon3) ** 2 - 1) + d3 * (d3 - 1) / delta ** 2) * exp(
-                -alpha3 * (delta - epsilon3) ** 2 - beta3 * (-gamma3 + tau) ** 2), axis=-1)
-            ddd_s4 = sum(bigdelta ** b4 * n4 * (2 * b4 * bigphi * dd_bigdelta / bigdelta + b4 * bigphi * delta * (
-                        b4 * dd_bigdelta ** 2 / bigdelta + ddd_bigdelta - dd_bigdelta ** 2 / bigdelta) / bigdelta + 2 *
-                                                b4 * dd_bigdelta * dd_bigphi * delta / bigdelta + 2 * dd_bigphi +
-                                                ddd_bigphi * delta), axis=-1)
-            res = ddd_s1 + ddd_s2 + ddd_s3 + ddd_s4
-        elif dt == 1 and dd == 1:
-            ddt_theta = 0
-            ddt_bigdelta = 2 * dd_theta * dt_theta + 2 * ddt_theta * theta
-            ddt_bigphi = 4 * C4 * D4 * (delta - 1) * (tau - 1) * exp(-C4 * (delta - 1) ** 2 - D4 * (tau - 1) ** 2)
-            ddt_s1 = sum(d1 * delta ** (d1 - 1) * n1 * t1 * tau ** (t1 - 1), axis=-1)
-            ddt_s2 = sum(delta ** (d2 - 1) * n2 * t2 * tau ** (t2 - 1) * (-c2 * delta ** c2 + d2) * exp(-delta ** c2),
-                         axis=-1)
-            ddt_s3 = sum(delta ** d3 * n3 * tau ** t3 * (
-                        -4 * alpha3 * beta3 * (delta - epsilon3) * (gamma3 - tau) - 2 * alpha3 * t3 * (
-                            delta - epsilon3) / tau + 2 * beta3 * d3 * (gamma3 - tau) / delta + d3 * t3 / (
-                                    delta * tau)) * exp(
-                -alpha3 * (delta - epsilon3) ** 2 - beta3 * (-gamma3 + tau) ** 2), axis=-1)
-            ddt_s4 = sum(bigdelta ** b4 * n4 * (
-                        b4 ** 2 * bigphi * dd_bigdelta * delta * dt_bigdelta / bigdelta ** 2 + b4 * bigphi *
-                        ddt_bigdelta * delta / bigdelta + b4 * bigphi * dt_bigdelta / bigdelta + b4 * dd_bigdelta *
-                        delta * dt_bigphi / bigdelta + b4 * dd_bigphi * delta * dt_bigdelta / bigdelta - b4 * bigphi *
-                        dd_bigdelta * delta * dt_bigdelta / bigdelta ** 2 + ddt_bigphi * delta + dt_bigphi), axis=-1)
-            res = ddt_s1 + ddt_s2 + ddt_s3 + ddt_s4
-        else:
-            raise NotImplementedError
+    res = equations.residual_helmholtz_energy(delta, tau, dd, dt)
+
     if return_scalar:
         return res[0]
     else:
